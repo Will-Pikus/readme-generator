f085372ead686cdb1ffe6e6fb3ae9f6ecb63a004
Robert Bullfrog <williampikus@gmail.com>
2022-02-14 19:53:35 -0800

    Created init and writeToFile Functions - added packages needed

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMD = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Commit logged!'))
+};
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data){
+        writeToFile("ReadMe-Generator", generateMD(data))
+    })
+}
 
 // Function call to initialize app
 init();
